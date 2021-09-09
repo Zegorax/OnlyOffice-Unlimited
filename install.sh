9b29cf32a49603a84f5f4ae6fd74b0fc6e8bb2fc
SmallGaoX <37288739+SmallGaoX@users.noreply.github.com>
2021-09-09 11:14:24 +0800

    update

--- install.sh
+++ install.sh
@@ -4,9 +4,9 @@ FILE=OO_PubKey
 if test -f "$FILE"; then
   echo Patch has already been applied. Starting DocumentServer...
 else
-  apt-get update && apt-get install -y python3.6-dev 
+  apt-get update && apt-get install -y python3
     wget https://bootstrap.pypa.io/get-pip.py
-    python3.6 get-pip.py
+    python3 get-pip.py
     pip install pycrypto
     rm -f /var/www/onlyoffice/Data/license.lic
     
@@ -87,7 +87,7 @@ for file in files:
 
 EOF
 
-    python3.6 index.py
+    python3 index.py
 
     echo Patching docservice and convert...
 
